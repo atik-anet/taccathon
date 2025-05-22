cde8ff300d236c0c0151fbfe7db3d044c366fd0d
pganesh <pganesh@arista.com>
2025-05-22 07:14:42 -0700

    feat: basic mcp server setup with fastapi

--- arista-mcp-server/main.py
+++ arista-mcp-server/main.py
@@ -1,21 +1,54 @@
 # server.py
 from mcp.server.fastmcp import FastMCP
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+ip = "10.224.23.44"
+port = "8000"
+
+@app.get("/test")
+async def root():
+    return {"message": "Hello World"}
 
 # Create an MCP server
 mcp = FastMCP("Demo")
 
-
-# Add an addition tool
 @mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
+async def get_tree_folders(directory: str) -> str:
+    """
+    Get the directory tree structure of a given directory.
+    
+    Args:
+        directory: The directory path to explore.
+    
+    Returns:
+        The text output of the 'tree' command representing the directory structure.
+    """
+    url = f"http://{ip}:{port}/tree?directory=SandTunnel"
 
-# Add a dynamic greeting resource
-@mcp.resource("greeting://{name}")
-def get_greeting(name: str) -> str:
-    """Get a personalized greeting"""
-    return f"Hello, {name}!"
+    try:
+        response = requests.get(url)  # or requests.post(url, json=payload) if it requires POST
+        response.raise_for_status()  # Raises HTTPError if the response was an error
+        data = response.json()  # or response.text if the response isn't JSON
+        return data
+    except requests.exceptions.RequestException as e:
+       print(e)
+       print(url)
+       return {"exception":"couldnot find"}
 
-def __main__():
-    mcp.run(transport="stdio")
+@mcp.tool()
+async def get_weather(city: str) -> str:
+    """
+     Get the weather information for a specified city.
+
+     Args:
+         city (str): The name of the city to get weather information for.
+
+     Returns:
+         str: A message containing the weather information for the specified city.
+     """
+    return f"The weather in {city} is sunny."
+
+
+app.mount("/", mcp.sse_app())
